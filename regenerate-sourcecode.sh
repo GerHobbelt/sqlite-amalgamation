2958cbdcfb67dd734e765c8d8085963b5afc7dc5
Ger Hobbelt <ger@hobbelt.com>
2022-01-06 14:58:14 +0100

    add shell script which executes the SQLite amalgamation build commands on our rig: just run the script, no need to remember the exact commands this way. :-)

---
+++ regenerate-sourcecode.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# SQLite source tree is assumed to sit in    ../sqlite/
+# 
+
+REPODIR=$( pwd )
+echo "REPODIR=$REPODIR"
+
+cd ../sqlite/
+make clean
+make amalgamation
+
+echo "Copy generated files to amalgamation repo in $REPODIR..."
+cp shell.c sqlite3.[1ch] sqlite3_analyzer.c sqlite3_checker.c sqlite3ext.* sqlite3rc.* sqlite3session.*  $REPODIR
